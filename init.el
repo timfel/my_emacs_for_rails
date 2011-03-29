06f814c39b03251f7acc572d7058bb0a6eddffdd
Tim Felgentreff <timfelgentreff@gmail.com>
2011-03-29 14:20:35 +0200

    Fixup nuke-all-buffers method. Imperative style just isn't LISP

--- init.el
+++ init.el
@@ -148,31 +148,25 @@
           (setq this-command 'centerer1))))
 
 
-;; Kills live buffers, leaves some emacs work buffers
-;; optained from http://www.chrislott.org/geek/emacs/dotemacs.html
-(defun nuke-some-buffers (&optional list)
+;; Kills some live buffers, leaves some emacs work buffers and the currently active tab
+(defun kill-all-but-active-buffers (&optional list)
   "For each buffer in LIST, kill it silently if unmodified. Otherwise ask.
 LIST defaults to all existing live buffers."
   (interactive)
-  (if (null list)
-      (setq list (buffer-list)))
-  (while list
-    (let* ((buffer (car list))
-           (name (buffer-name buffer)))
-      (and (not (string-equal name ""))
-           ;(not (string-equal name "*Messages*"))
-          ;; (not (string-equal name "*Buffer List*"))
-           ;(not (string-equal name "*buffer-selection*"))
-           ;(not (string-equal name "*Shell Command Output*"))
-           (not (string-equal name "*scratch*"))
-           (/= (aref name 0) ? )
-           (if (buffer-modified-p buffer)
-               (if (yes-or-no-p
-                    (format "Buffer %s has been edited. Kill? " name))
-                   (kill-buffer buffer))
-             (kill-buffer buffer))))
-    (setq list (cdr list))))
-
+  (if (null list) (setq list (buffer-list)))
+  (dolist (buffer list)
+    (let ((name (buffer-name buffer)))
+      (if (and (not (string-equal (buffer-name) name)) ;; Don't kill the active buffer
+               (notany
+                (lambda (x) (string-match-p x name))
+                '("^$" "\\*Messages\\*" "\\*Buffer List\\*" "\\*buffer-selection\\*"
+                  "\\*Shell Command Output\\*" "\\*scratch\\*" "ECB" "magit"))
+               (/= (aref name 0) ? ))
+          (if (buffer-modified-p buffer)
+              (if (yes-or-no-p
+                   (format "Buffer %s has been edited. Kill? " name))
+                  (kill-buffer buffer))
+            (kill-buffer buffer))))))
 
 ;; fullscreen
 ;(defun toggle-fullscreen ()
