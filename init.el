4fbb477b22256dc99499262a8c76e82ef43bffe1
Tim Felgentreff <timfelgentreff@gmail.com>
2011-09-27 19:55:30 -0700

    cleanup

--- init.el
+++ init.el
@@ -26,184 +26,189 @@
      (end-of-buffer)
      (eval-print-last-sexp))))
 
-;; autotest
+(require 'compile) ;; Needed for some reason or other. el-get fails for me, otherwise
 
 (setq el-get-sources
-      '(cssh el-get switch-window vkill yasnippet
-             google-maps xcscope anything auto-complete
+      '(
+	
+	magithub gist ruby-electric autopair haml-mode nxhtml
+	rspec-mode sass-mode cssh el-get switch-window vkill
+	yasnippet xcscope anything auto-complete sudo-save
 
-             (:name redo+
-                    :type http
-                    :url "http://www.emacswiki.org/emacs/download/redo%2b.el"
-                    :load "redo%2b.el"
-                    :post-init (lambda () (global-set-key [(control -)] 'redo)))
+	(:name redo+
+	       :type http
+	       :url "http://www.emacswiki.org/emacs/download/redo%2b.el"
+	       :load "redo%2b.el"
+	       :post-init (lambda () (global-set-key [(control -)] 'redo)))
 
-             (:name ecb
-                    :load-path "."
-                    :features ecb
-                    :post-init (lambda () (progn
-                                            (global-ede-mode 1)
-                                            (semantic-mode t)
-                                            (setq semantic-load-turn-everything-on t)
-                                            (setq ecb-tip-of-the-day nil)
-                                            (setq ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2))
-                                            (if window-system
-                                                (if (>= (window-height) 16)
-                                                    (progn
-                                                      (ecb-activate)
-                                                      (add-hook 'window-setup-hook 'ecb-redraw-layout t))
-                                                  (message "Not activating ECB, window height to small"))
-                                              (message "Not activating ECB, not using a window system")))))
+	(:name textlint
+	       :type git
+	       :url "git://github.com/DamienCassou/textlint.git"
+	       :load "textlint.el")
 
-             ;; auto-complete-clang auto-complete-etags auto-complete-extensions
+	(:name ecb
+	       :load-path "."
+	       :features ecb
+	       :post-init (lambda () (progn
+				       (global-ede-mode 1)
+				       (semantic-mode t)
+				       (setq semantic-load-turn-everything-on t)
+				       (setq ecb-tip-of-the-day nil)
+				       (setq ecb-primary-secondary-mouse-buttons (quote mouse-1--mouse-2))
+				       (if window-system
+					   (if (>= (window-height) 16)
+					       (progn
+						 (ecb-activate)
+						 (add-hook 'window-setup-hook 'ecb-redraw-layout t))
+					     (message "Not activating ECB, window height to small"))
+					 (message "Not activating ECB, not using a window system")))))
 
-             (:name color-theme
-                    :load-path "."
-                    :load "color-theme.el")
-             (:name color-theme-solarized
-                    :depends color-theme
-                    :type git
-                    :url "https://github.com/sellout/emacs-color-theme-solarized.git"
-                    :load "color-theme-solarized.el"
-                    :post-init (lambda () (color-theme-solarized-light)))
+	;; auto-complete-clang auto-complete-etags auto-complete-extensions
 
-             (:name coffee-mode
-                    :post-init (lambda () (add-hook 'coffee-mode-hook
-                                                    '(lambda() (progn
-                                                                 (set (make-local-variable 'tab-width) 2)
-                                                                 (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer))))))
+	(:name color-theme
+	       :load-path "."
+	       :load "color-theme.el")
+	(:name color-theme-solarized
+	       :depends color-theme
+	       :type git
+	       :url "https://github.com/sellout/emacs-color-theme-solarized.git"
+	       :load "color-theme-solarized.el"
+	       :post-init (lambda () (color-theme-solarized-light)))
 
-             autopair haml-mode
-             nxhtml
-             rspec-mode
-             sass-mode
+	(:name coffee-mode
+	       :post-init (lambda () (add-hook 'coffee-mode-hook
+					       '(lambda() (progn
+							    ;; Enable compile-on-save if there is already a *.coffee & *.js file
+							    (if (and (file-exists-p (buffer-file-name))
+								     (file-exists-p (coffee-compiled-file-name)))
+								(coffee-cos-mode t))
+							    (setq coffee-args-compile '("-c" "--bare"))
+							    (set (make-local-variable 'tab-width) 2)
+							    (define-key coffee-mode-map [(meta r)] 'coffee-compile-buffer))))))
 
-             sudo-save
+	(:name yaml-mode
+	       :post-init (lambda () (progn
+				       (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+				       (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+				       (add-to-list 'auto-mode-alist '("Gemfile.lock$" . yaml-mode)))))
 
-             (:name yaml-mode
-                    :post-init (lambda () (progn
-                                            (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-                                            (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
-                                            (add-to-list 'auto-mode-alist '("Gemfile.lock$" . yaml-mode)))))
+	(:name maxframe
+	       :features maxframe
+	       :post-init (lambda () (add-hook 'window-setup-hook 'maximize-frame t)))
 
-             (:name maxframe
-                    :features maxframe
-                    :post-init (lambda () (add-hook 'window-setup-hook 'maximize-frame t)))
+	;; (:name rinari
+	;;       :post-init (lambda () (setq rinari-tags-file-name "TAGS")))
 
-             (:name rinari
-                    :post-init (lambda () (setq rinari-tags-file-name "TAGS")))
+	(:name ri-emacs
+	       :post-init (lambda () (setq ri-ruby-script (expand-file-name (concat el-get-dir "/ri-emacs/ri-emacs.rb")))))
 
-             (:name ri-emacs
-                    :post-init (lambda () (setq ri-ruby-script (expand-file-name (concat el-get-dir "/ri-emacs/ri-emacs.rb")))))
+	(:name rhtml-mode
+	       :features rhtml-mode
+	       :post-init (lambda () (progn
+				       ;; (add-hook 'rhtml-mode-hook
+				;;		 (lambda () (rinari-launch)))
+				       (add-to-list 'auto-mode-alist '("\\.html.erb$" . rhtml-mode))
+				       (add-to-list 'auto-mode-alist '("\\.html.rb$" . rhtml-mode))
+				       (add-to-list 'auto-mode-alist '("\\.rhtml$" . rhtml-mode))
+				       )))
 
-             (:name rhtml-mode
-                    :features rhtml-mode
-                    :post-init (lambda () (progn
-                                            (add-hook 'rhtml-mode-hook
-                                                      (lambda () (rinari-launch)))
-                                            (add-to-list 'auto-mode-alist '("\\.html.erb$" . rhtml-mode))
-                                            (add-to-list 'auto-mode-alist '("\\.html.rb$" . rhtml-mode))
-                                            (add-to-list 'auto-mode-alist '("\\.rhtml$" . rhtml-mode))
-                                            )))
+	(:name ruby-mode
+	       :after (lambda () (progn
+				   (add-hook 'ruby-mode-hook 'turn-on-font-lock)
+				   (add-hook 'ruby-mode-hook '(lambda() ((add-hook 'write-contents-functions
+										   '(lambda()
+										      (save-excursion
+											(untabify (point-min) (point-max))
+											(delete-trailing-whitespace)
+											)))
+									 (ruby-electric-mode t)
+									 (ruby-block-mode t)
+									 ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
+									 (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
+									     (flymake-mode))
+									 ;; Indenting options
+									 (set (make-local-variable 'indent-tabs-mode) 'nil)
+									 (set (make-local-variable 'tab-width) 2)
+									 (local-set-key (kbd "<return>") 'newline-and-indent)
+									 ;; Auto completion
+									 (imenu-add-to-menubar "IMENU")
+									 (setq ac-sources (append '(ac-source-rsense-method ac-source-rsense-constant) ac-sources))
+									 (setq ac-sources (append ac-sources '(ac-source-words-in-same-mode-buffers)))
+									 (setq ac-omni-completion-sources '(("\\.\\=" ac-source-rcodetools)))
+									 (local-set-key "\M-\C-i" 'ri-ruby-complete-symbol)
+									 (define-key ruby-mode-map "\M-\C-o" 'rct-complete-symbol)
+									 ;; Type inference auto completion
+									 (if (project-current)
+									     (rsense-open-project (project-default-directory (project-current)))))))
+				   (add-to-list 'auto-mode-alist '("\\.rjs$" . ruby-mode))
+				   (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+				   (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+				   (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+				   (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode)))))
 
-             (:name ruby-mode
-                    :after (lambda () (progn
-                                        (add-hook 'ruby-mode-hook 'turn-on-font-lock)
-                                        (add-hook 'ruby-mode-hook '(lambda() ((add-hook 'write-contents-functions
-                                                                                        '(lambda()
-                                                                                           (save-excursion
-                                                                                             (untabify (point-min) (point-max))
-                                                                                             (delete-trailing-whitespace)
-                                                                                             )))
-                                                                              (ruby-electric-mode t)
-                                                                              (ruby-block-mode t)
-                                                                              ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
-                                                                              (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
-                                                                                  (flymake-mode))
-                                                                              ;; Indenting options
-                                                                              (set (make-local-variable 'indent-tabs-mode) 'nil)
-                                                                              (set (make-local-variable 'tab-width) 2)
-                                                                              (local-set-key (kbd "<return>") 'newline-and-indent)
-                                                                              ;; Auto completion
-                                                                              (imenu-add-to-menubar "IMENU")
-                                                                              (setq ac-sources (append '(ac-source-rsense-method ac-source-rsense-constant) ac-sources))
-                                                                              (setq ac-sources (append ac-sources '(ac-source-words-in-same-mode-buffers)))
-                                                                              (setq ac-omni-completion-sources '(("\\.\\=" ac-source-rcodetools)))
-                                                                              (local-set-key "\M-\C-i" 'ri-ruby-complete-symbol)
-                                                                              (define-key ruby-mode-map "\M-\C-o" 'rct-complete-symbol)
-                                                                              ;; Type inference auto completion
-                                                                              (if (project-current)
-                                                                                  (rsense-open-project (project-default-directory (project-current)))))))
-                                        (add-to-list 'auto-mode-alist '("\\.rjs$" . ruby-mode))
-                                        (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
-                                        (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
-                                        (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
-                                        (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode)))))
-             ruby-electric
-             (:name cucumber
-                    :type git
-                    :url "git://github.com/michaelklishin/cucumber.el.git"
-                    :load-path "."
-                    :features feature-mode
-                    :depends yasnippet
-                    :post-init (lambda () (progn
-                                            ;; load bundle snippets
-                                            (yas/load-directory (expand-file-name (concat el-get-dir "/cucumber/snippets")))
-                                            (add-to-list 'auto-mode-alist '("\\.feature" . feature-mode)))))
+	(:name cucumber
+	       :type git
+	       :url "git://github.com/michaelklishin/cucumber.el.git"
+	       :load-path "."
+	       :features feature-mode
+	       :depends '(yasnippet)
+	       :post-init (lambda () (progn
+				       ;; load bundle snippets
+				       (yas/load-directory (expand-file-name (concat el-get-dir "/cucumber/snippets")))
+				       (add-to-list 'auto-mode-alist '("\\.feature" . feature-mode)))))
 
-             (:name rsense
-                    :type git
-                    :url "git://github.com/m2ym/rsense.git"
-                    :prepare (lambda () (setq rsense-home (expand-file-name (concat el-get-dir "/rsense"))))
-                    :features rsense
-                    :load-path "etc")
+	(:name rsense
+	       :type git
+	       :url "git://github.com/m2ym/rsense.git"
+	       :prepare (lambda () (setq rsense-home (expand-file-name (concat el-get-dir "/rsense"))))
+	       :features rsense
+	       :load-path "etc")
 
-             (:name js2-mode
-                    :post-init (lambda () (progn
-                                            (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-                                            (add-hook 'js2-mode-hook
-                                                      (lambda () (progn
-                                                                   (setq imenu-create-index-function 'javascript-imenu-create-index)
-                                                                   (local-set-key (kbd "<return>") 'newline-and-indent)
-                                                                   (setq javascript-indent-level 2)))
-                                                      t))))
+	(:name js2-mode
+	       :post-init (lambda () (progn
+				       (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+				       (add-hook 'js2-mode-hook
+						 (lambda () (progn
+							      (setq imenu-create-index-function 'javascript-imenu-create-index)
+							      (local-set-key (kbd "<return>") 'newline-and-indent)
+							      (setq javascript-indent-level 2)))
+						 t))))
 
-             (:name textmate
-                    :type git
-                    :url "git://github.com/defunkt/textmate.el.git"
-                    :load "textmate.el"
-                    :post-init (lambda () (textmate-mode 1)))
+	(:name textmate
+	       :type git
+	       :url "git://github.com/defunkt/textmate.el.git"
+	       :load "textmate.el"
+	       :post-init (lambda () (textmate-mode 1)))
 
-             (:name magit
-                    :features (magit magit-svn)
-                    :post-init (lambda () (global-set-key (kbd "C-x C-z") 'magit-status)))
-             magithub
-             gist
+	(:name magit
+	       :features (magit magit-svn)
+	       :post-init (lambda () (global-set-key (kbd "C-x C-z") 'magit-status)))
 
-             (:name project-mode
-                    :type git
-                    :url "http://github.com/timfel/emacs-project-mode.git"
-                    :load-path "."
-                    :features project-mode
-                    :post-init (lambda () (progn
-                                            (project-mode 1)
-                                            (project-mode-menu)
-                                            (project-load-all)
-                                            (global-set-key "\C-t" 'project-fuzzy-search))))
+	(:name project-mode
+	       :type git
+	       :url "http://github.com/timfel/emacs-project-mode.git"
+	       :load-path "."
+	       :features project-mode
+	       :post-init (lambda () (progn
+				       (project-mode 1)
+				       (project-mode-menu)
+				       (project-load-all)
+				       (global-set-key "\C-t" 'project-fuzzy-search))))
 
-             ;; (:name dictionary-el    :type apt-get)
-             ;; (:name emacs-goodies-el :type apt-get)
+	;; (:name dictionary-el    :type apt-get)
+	;; (:name emacs-goodies-el :type apt-get)
 
-             (:name org-mode
-                    :post-init (lambda () (progn
-                                            (setq org-hide-leading-stars t)
-                                            (setq org-agenda-files '())
-                                            (add-to-list 'org-agenda-files (expand-file-name "~/Desktop"))
-                                            (setq org-agenda-files (append (file-expand-wildcards
-                                                                            (expand-file-name "~/Documents/HPI/11SS/*"))
-                                                                           org-agenda-files))
-                                            (setq org-insert-mode-line-in-empty-file t))))
-             ))
+	(:name org-mode
+	       :post-init (lambda () (progn
+				       (setq org-hide-leading-stars t)
+				       (setq org-agenda-files '())
+				       (add-to-list 'org-agenda-files (expand-file-name "~/Desktop"))
+				       (setq org-agenda-files (append (file-expand-wildcards
+								       (expand-file-name "~/Documents/HPI/11SS/*"))
+								      org-agenda-files))
+				       (setq org-insert-mode-line-in-empty-file t))))
+
+	))
 
 ;; Auctex depends on pdflatex being available, only install if desired on this system
 (if (executable-find "pdflatex")
@@ -228,8 +233,17 @@
                                                    (setq-default TeX-master nil)))))
                   el-get-sources)))
 
-(setq my-packages (mapcar 'el-get-source-name el-get-sources))
-(el-get 'sync my-packages)
+;; (setq el-get-sources '())
+;; (setq my-packages
+;;       (mapcar 'el-get-as-symbol
+;; 	      (append '(magithub gist ruby-electric autopair
+;; 				 haml-mode nxhtml rspec-mode
+;; 				 sass-mode cssh el-get
+;; 				 switch-window vkill yasnippet
+;; 				 xcscope anything auto-complete
+;; 				 sudo-save)
+;; 		      (mapcar 'el-get-source-name el-get-sources))))
+(el-get 'sync)
 
 ;; stops me killing emacs by accident!
 (setq confirm-kill-emacs 'yes-or-no-p)
@@ -596,7 +610,7 @@ makes)."
              ))
 
 ;; yasnippet rails
-(load "~/.emacs.d/plugins/yasnippets-rails/setup.el")
+;; (load "~/.emacs.d/plugins/yasnippets-rails/setup.el")
 
 (add-to-list 'load-path "~/.emacs.d/plugins/autotest")
 (require 'autotest)
