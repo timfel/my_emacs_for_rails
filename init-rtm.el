134fb73b96c2f96f05f1de8fb7e58edb62e897ed
Tim Felgentreff <timfelgentreff@gmail.com>
2011-11-24 12:56:11 +0100

    More useful RTM org-mode integration

--- init-rtm.el
+++ init-rtm.el
@@ -2,12 +2,16 @@
   (interactive)
   (el-get-init "slack-rtm")
   (if (eq nil (get-buffer "*todo*"))
-      (slack-rtm slack-rtm-default-query)
-    (switch-to-buffer-other-window "*todo*")))
+      (slack-rtm slack-rtm-default-query))
+  (switch-to-buffer-other-window "*todo*"))
 
 (global-set-key "\C-x\C-g" 'gtd)
 
 (add-hook 'org-mode-hook (lambda ()
-			   (local-set-key (kbd "C-c C-x t") 'slack-rtm-sync-task)
-			   (local-set-key (kbd "C-c C-x d") 'slack-rtm-delete-task)
-			   (local-set-key (kbd "C-c C-x s") 'slack-rtm-sync)))
+			   (if (string= (buffer-name) "*todo*")
+			       (progn
+				 (local-set-key (kbd "<S-right>") (lambda () (interactive) (slack-rtm-touch-task) (org-shiftright)))
+				 (local-set-key (kbd "<S-left>") (lambda () (interactive) (slack-rtm-touch-task) (org-shiftleft)))
+				 (local-set-key (kbd "C-x C-s") 'slack-rtm-sync)
+				 (local-set-key (kbd "C-x C-d") 'slack-rtm-delete-task)
+				 (local-set-key (kbd "C-t") 'slack-rtm-touch-task)))))
