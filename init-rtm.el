5e485c3a0de0977f66c5fa51c511ad7b2d5c72ea
Tim Felgentreff <timfelgentreff@gmail.com>
2011-11-30 11:41:50 +0100

    * Reload the buffer on save, so we get the current view * Bind visit to reload-buffer

--- init-rtm.el
+++ init-rtm.el
@@ -5,13 +5,21 @@
       (slack-rtm slack-rtm-default-query))
   (switch-to-buffer-other-window "*todo*"))
 
+(defun slack-rtm-reload-buffer ()
+  (save-selected-window
+    (slack-rtm slack-rtm-default-query)
+    (buffer-swap-text (get-buffer "*todo*<2>"))
+    (kill-buffer "*todo*<2>")))
+
 (global-set-key "\C-x\C-g" 'gtd)
 
-(add-hook 'org-mode-hook (lambda ()
-			   (if (string= (buffer-name) "*todo*")
-			       (progn
-				 (local-set-key (kbd "<S-right>") (lambda () (interactive) (slack-rtm-touch-task) (org-shiftright)))
-				 (local-set-key (kbd "<S-left>") (lambda () (interactive) (slack-rtm-touch-task) (org-shiftleft)))
-				 (local-set-key (kbd "C-x C-s") 'slack-rtm-sync)
-				 (local-set-key (kbd "C-x C-d") 'slack-rtm-delete-task)
-				 (local-set-key (kbd "C-t") 'slack-rtm-touch-task)))))
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    (if (string= (buffer-name) "*todo*")
+		(progn
+		  (local-set-key (kbd "<S-right>") (lambda () (interactive) (slack-rtm-touch-task) (org-shiftright)))
+		  (local-set-key (kbd "<S-left>") (lambda () (interactive) (slack-rtm-touch-task) (org-shiftleft)))
+		  (local-set-key (kbd "C-x C-s") (lambda () (interactive) (slack-rtm-sync) (slack-rtm-reload-buffer)))
+		  (local-set-key (kbd "C-x C-v") 'slack-rtm-reload-buffer)
+		  (local-set-key (kbd "C-x C-d") 'slack-rtm-delete-task)
+		  (local-set-key (kbd "C-t") 'slack-rtm-touch-task)))))
