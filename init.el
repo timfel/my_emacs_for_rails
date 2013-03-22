49a50870dc5c0ac3be1f2e9ca11c3ebcdb7c5101
Tim Felgentreff <timfelgentreff@gmail.com>
2013-03-22 11:47:36 +0100

    pdflatex, always pdflatex

--- init.el
+++ init.el
@@ -14,6 +14,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(LaTeX-command "pdflatex")
  '(TeX-source-correlate-method (quote synctex))
  '(TeX-source-correlate-mode t)
  '(TeX-source-correlate-start-server t)
