3feb2e97c8800963e4ed21852f0e422d3bf3f027
Tim Felgentreff <timfelgentreff@gmail.com>
2015-04-25 11:32:50 +0200

    remove duplicate lines (except newlines)

--- init-useful-functions.el
+++ init-useful-functions.el
@@ -61,3 +61,21 @@ LIST defaults to all existing live buffers."
 ;; wc
 (defun word-count nil "Count words in buffer" (interactive)
   (shell-command-on-region (region-beginning) (region-end) "wc -w"))
+
+
+;; remove duplicate lines
+(defun uniquify-all-lines-region (start end)
+  "Find duplicate lines in region START to END keeping first occurrence."
+  (interactive "*r")
+  (save-excursion
+    (let ((end (copy-marker end)))
+      (while
+	  (progn
+	    (goto-char start)
+	    (re-search-forward "^\\(.+\\)\n\\(\\(.*\n\\)*\\)\\1\n" end t))
+	(replace-match "\\1\n\\2")))))
+
+(defun uniquify-all-lines-buffer ()
+  "Delete duplicate lines in buffer and keep first occurrence."
+  (interactive "*")
+  (uniquify-all-lines-region (point-min) (point-max)))
