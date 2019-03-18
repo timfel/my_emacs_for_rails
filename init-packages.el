355d17464aec06e7ca0d7cd6123e015f8d2f986c
Tim Felgentreff <timfelgentreff@gmail.com>
2019-03-18 15:49:06 +0100

    doom-modeline in java projects

--- init-packages.el
+++ init-packages.el
@@ -338,6 +338,7 @@
             (require 'lsp-ui-sideline)
             ;; (setq lsp-java-workspace-dir "/home/tim/eclipse-workspace/")
             (add-hook 'java-mode-hook #'lsp)
+            (add-hook 'java-mode-hook 'doom-modeline-mode)
             (add-hook 'java-mode-hook 'friendly-whitespace)
             (add-hook 'java-mode-hook (lambda () (flycheck-mode t)))
             (add-hook 'java-mode-hook (lambda () (company-mode t)))
