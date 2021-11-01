e2b6c3c7d26be7f8f69ce9081b9e894ca097db4d
Tim Felgentreff <tim.felgentreff@oracle.com>
2021-11-01 15:29:33 +0100

    update lsp jdk

--- init.el
+++ init.el
@@ -36,7 +36,7 @@
    "/home/tim/.emacs.d/c64debugger/c64debugger -autojmp -wait 4000")
  '(kickasm-vice-command "x64")
  '(lsp-graalvm-languages '("python" "ruby"))
- '(lsp-netbeans-jdk "/home/tim/graalvms/graalvm-ee-java17-21.3.0-dev/")
+ '(lsp-netbeans-jdk "/home/tim/graalvms/graalvm-ce-java17-21.3.0/")
  '(lsp-tcp-connection-timeout 20)
  '(lsp-ui-doc-position 'top)
  '(markdown-command "cmark-gfm --extension table")
