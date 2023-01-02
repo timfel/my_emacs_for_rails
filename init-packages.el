827b9e013348eed35d368585a5b690f8888a3496
Tim Felgentreff <timfelgentreff@gmail.com>
2023-01-02 12:41:48 +0100

    mx clean and mx build command functions

--- init-packages.el
+++ init-packages.el
@@ -492,6 +492,46 @@
   :bind ("<f5>" . treemacs-t)
   :after (lsp-mode company lsp-treemacs)
   :config (progn
+            (defun mxclean ()
+              (interactive)
+              (make-process
+                   :name "mxclean"
+                   :sentinel (lambda (proc _)
+                               (if (not (process-live-p proc))
+                                   (message "mx clean done")))
+                   :command `("/bin/bash" "-l" "-i" "-c" ,(format "cd %s; mx clean" (vc-root-dir)))))
+            (defun mxbuild ()
+              (interactive)
+              (lexical-let* ((progress-reporter
+                             (make-progress-reporter "Running mx build..."))
+                             (timer (run-with-timer
+                                     1
+                                     1
+                                     (lambda ()
+                                       (progress-reporter-update progress-reporter
+                                                                 (with-current-buffer (get-buffer "*mxbuild*")
+                                                                   (goto-char (point-max))
+                                                                   (forward-line -1)
+                                                                   (buffer-substring (point) (point-max))))))))
+                (if (get-buffer "*mxbuild*")
+                    (kill-buffer "*mxbuild*"))
+                (make-process
+                 :name "mxbuild"
+                 :buffer "*mxbuild*"
+                 :command `("/bin/bash" "-l" "-i" "-c" ,(format "cd %s; mx build" (vc-root-dir)))
+                 :sentinel (lambda (proc _)
+                             (if (not (process-live-p proc))
+                                 (progn
+                                   (cancel-timer timer)
+                                   (progress-reporter-done progress-reporter)
+                                   (run-with-timer 2
+                                                   nil
+                                                   (lambda ()
+                                                     (message
+                                                      (with-current-buffer (get-buffer "*mxbuild*")
+                                                        (goto-char (point-max))
+                                                        (forward-line -5)
+                                                        (buffer-substring (point) (point-max))))))))))))
             (require 'lsp-ui-flycheck)
             (require 'lsp-ui-sideline)
             (setq lsp-java-completion-favorite-static-members (vconcat lsp-java-completion-favorite-static-members
