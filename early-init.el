909b735863d7cc2905736dd365e99f54f06584b9
Tim Felgentreff <timfelgentreff@gmail.com>
2026-08-04 15:40:09 +0200

    document

--- early-init.el
+++ early-init.el
@@ -1,9 +1,12 @@
 ;;; -*- lexical-binding: t -*-
-(setq load-prefer-newer t)
-(setenv "LSP_USE_PLISTS" "true")
+
+(setq load-prefer-newer t)       ;; do not trip up when .el is newer than .elc
+
+(setenv "LSP_USE_PLISTS" "true") ;; if we load lsp-mode, this makes it faster
 (defvar lsp-use-plists)
 (setq lsp-use-plists t)
-(when (eq system-type 'android)
+
+(when (eq system-type 'android)  ;; the android version works best with termux
   (let ((termuxpath "/data/data/com.termux/files/usr/bin"))
     (setenv "PATH" (concat (getenv "PATH") ":"  termuxpath))
     (add-to-list 'exec-path termuxpath t)))
